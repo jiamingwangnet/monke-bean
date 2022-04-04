97aa8015cfc1e9e2d2c492c92ab86dbaeab5302d
jiamingwangnet <jiamingwang.net@gmail.com>
2022-04-04 18:06:11 +1000

    display character range

--- src/main.js
+++ src/main.js
@@ -4,13 +4,13 @@ const desiredResult = "bean";
 const typedCharacters = [];
 
 const charsets = {
-    speedy: [0x61, 0x6E],
-    easy: [0x61, 0x7A],
-    cheap: [0x41, 0x7A],
-    simple: [0x24, 0x7E],
-    difficult: [0x00, 0xff],
-    extended: [0x00, 0xffff],
-    unicode: [0x00, 0x10ffff]
+    speedy:    [0x000061, 0x00006E],
+    easy:      [0x000061, 0x00007A],
+    cheap:     [0x000041, 0x00007A],
+    simple:    [0x000024, 0x00007E],
+    difficult: [0x000000, 0x0000ff],
+    extended:  [0x000000, 0x00ffff],
+    unicode:   [0x000000, 0x10ffff]
 }
 
 function getRandomCharacter(min, max) {
@@ -85,10 +85,40 @@ function pageScroll() {
 function writeChance() {
     const charset = document.querySelector("#charset").value;
     const chanceText = document.querySelector("#chance");
+    const rangeText = document.querySelector("#range");
 
     let chance = (charsets[charset][1] - charsets[charset][0] + 1) ** desiredResult.length;
 
+    let formatedRange01 = "";
+    let formatedRange02 = "";
+
+    for(let i = 0; i < 6; i++)
+    {
+        if(6 - i > charsets[charset][0].toString(16).length)
+        {
+            formatedRange01 += "0";
+        }
+        else
+        {
+            formatedRange01 += charsets[charset][0].toString(16)[i - (6 - charsets[charset][0].toString(16).length)];
+        }
+    }
+
+    for(let i = 0; i < 6; i++)
+    {
+        if(6 - i > charsets[charset][1].toString(16).length)
+        {
+            formatedRange02 += "0";
+        }
+        else
+        {
+            formatedRange02 += charsets[charset][1].toString(16)[i - (6 - charsets[charset][1].toString(16).length)];
+        }
+    }
+
+
     chanceText.innerHTML = `1 in <b>${chance}</b> chance`;
+    rangeText.innerHTML = `Range: U+${formatedRange01} - U+${formatedRange02}`;
 }
 
 window.onload = writeChance;
